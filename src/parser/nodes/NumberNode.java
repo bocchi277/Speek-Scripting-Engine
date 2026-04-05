304a6d9d4cfd2b4a7fd8281bd1b414bf5adcf5e9
Aditya <aadik7212@gmail.com>
2026-04-05 11:23:38 +0530

    Update NumberNode.java

--- src/parser/nodes/NumberNode.java
+++ src/parser/nodes/NumberNode.java
@@ -1,1 +1,14 @@
+package parser.nodes;
+
+import parser.Expression;
+import evaluator.Environment;
+
+public class NumberNode implements Expression {
+    private final double value;
+    public NumberNode(double value) { this.value = value; }
+
+    public Object evaluate(Environment env) {
+        return value;
+    }
+}
 
