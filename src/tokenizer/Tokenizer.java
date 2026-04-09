c318ca6fc0f3d7987cd277f24c5eb45e0ae1ebfa
1212ekta <ektasaw14@gmail.com>
2026-04-09 23:21:03 +0530

    Added keyword and phrase maps

--- src/tokenizer/Tokenizer.java
+++ src/tokenizer/Tokenizer.java
@@ -18,4 +18,22 @@ public class Tokenizer {
         this.pos = 0;
         this.line = 1;
     }
+
+        // Keywords (Open/Closed Principle)
+    private static final Map<String, TokenType> keywords = new HashMap<>();
+
+    // Phrases (like "is greater than")
+    private static final Map<String, TokenType> phrases = new HashMap<>();
+
+    static {
+        keywords.put("let", TokenType.LET);
+        keywords.put("be", TokenType.BE);
+        keywords.put("say", TokenType.SAY);
+        keywords.put("if", TokenType.IF);
+        keywords.put("then", TokenType.THEN);
+        keywords.put("repeat", TokenType.REPEAT);
+        keywords.put("times", TokenType.TIMES);
+
+        phrases.put("is greater than", TokenType.GREATER_THAN);
+    }
 }
