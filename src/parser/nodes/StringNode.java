5297aa157c3f8be9c51246dea5d694f9b1633eca
Aditya <aadik7212@gmail.com>
2026-04-14 19:49:48 +0530

    Update StringNode.java
    Add Comment

--- src/parser/nodes/StringNode.java
+++ src/parser/nodes/StringNode.java
@@ -1,13 +1,35 @@
-
 package parser.nodes;
 
 import parser.Expression;
 import evaluator.Environment;
 
+/**
+ * StringNode represents a string literal in the AST (Abstract Syntax Tree).
+ * 
+ * Examples:
+ * - "hello"
+ * - "world"
+ * 
+ * This node stores a string value and returns it during evaluation.
+ */
 public class StringNode implements Expression {
-    private final String value;
-    public StringNode(String value) { this.value = value; }
 
+    // Stores the string value
+    private final String value;
+
+    // Constructor to initialize string
+    public StringNode(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Evaluates the string node.
+     * 
+     * Since this is a constant value, it simply returns the string.
+     * 
+     * @param env Environment (not used here, but required by interface)
+     * @return string value
+     */
     public Object evaluate(Environment env) {
         return value;
     }
